66d7173d32756d7f4b50eedbf22cffba7903b444
cuongledev <cuongle.dev@gmail.com>
2016-12-22 02:25:52 +0700

    Updated by cuongle

--- public/js/main/app.js
+++ public/js/main/app.js
@@ -2,4 +2,30 @@ var app = angular.module('app.todos', []);
 
 app.controller("todoController",['$scope',function($scope){
     $scope.appName = "TODOS DASHBOARD";
+    $scope.formData = {};
+    $scope.todos = [
+
+        {
+            text: "Cài đặt Angularjs,bootstrap,fontawesome...",
+            isDone: true
+        },{
+            text: "Kết nôi Nodejs Server",
+            isDone: true
+        },{
+            text: "Data binding vào teamplate..",
+            isDone: false
+        },{
+            text: "Deploy app lên hedoku..",
+            isDone: false
+        }
+    ];
+
+    $scope.createTodo = function(){
+        var todo = {
+            text: $scope.formData.text,
+            isDone: false
+        };
+        $scope.todos.push(todo);
+        $scope.formData.text = "";
+    }
 }]);
